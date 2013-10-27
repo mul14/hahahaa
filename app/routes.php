9c742c35254d3c588b3145049b270bfcb59c2bad
Mulia Arifandy Nasution <mul14.net@gmail.com>
2013-10-27 11:23:32 +0700

    Update the routes

--- app/routes.php
+++ app/routes.php
@@ -4,3 +4,13 @@ Route::get('/', [
     'as' => 'home',
     'uses' => 'HomeController@showWelcome'
 ]);
+
+Route::get('login', ['as' => 'login', 'uses' => 'UserController@login']);
+Route::post('login', ['as' => 'auth', 'uses' => 'UserController@auth']);
+Route::get('logout', ['as' => 'logout', 'uses' => 'UserController@logout']);
+Route::get('profile', ['as' => 'profile', 'uses' => 'UserController@profile']);
+
+Route::resource('user', 'UserController');
+
+Route::resource('project', 'ProjectController');
+Route::resource('project.task', 'TaskController');
